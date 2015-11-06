1601a21eec5d3d3318058f33f27b7d98cad2235a
youfuli <youfuli.fly@gmail.com>
2015-11-05 21:15:33 -0800

    revision

--- src/main/java/org/logsystems/corfudb/CorfuDBMultiple.java
+++ src/main/java/org/logsystems/corfudb/CorfuDBMultiple.java
@@ -3,7 +3,7 @@ package org.logsystems.corfudb;
 import org.corfudb.infrastructure.NettyLogUnitServer;
 import org.corfudb.infrastructure.NettyStreamingSequencerServer;
 import org.corfudb.runtime.CorfuDBRuntime;
-import org.corfudb.runtime.OutOfSpaceException;
+
 import org.corfudb.runtime.stream.*;
 import org.corfudb.runtime.view.*;
 import org.corfudb.util.CorfuDBFactory;
@@ -26,64 +26,45 @@ public class CorfuDBMultiple{
     CorfuDBRuntime cdr;
     ICorfuDBInstance cinstance;
     public CorfuDBMultiple(String master,String serviceURL){
-//        opts.put(master,serviceURL);
-//        cdbFactory=new CorfuDBFactory(opts);
-//        cdr=cdbFactory.getRuntime();
-//        cinstance=cdr.getLocalInstance();
-//        cinstance.getConfigurationMaster().resetAll();
-        Map<String, Object> luConfigMap = new HashMap<String,Object>() {
-            {
-                put("capacity", 100000);
-                put("ramdisk", true);
-                put("pagesize", 4096);
-                put("trim", 0);
-            }
-        };
-        CorfuInfrastructureBuilder cbuilder = new CorfuInfrastructureBuilder().
-                addLoggingUnit(7002, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
-                addLoggingUnit(7003, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
-                addLoggingUnit(7004, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
-                addSequencer(7001, NettyStreamingSequencerServer.class, "nsss", null).
-                //              setReplication("cdbqr").
-                start(9999) ;
-
-        cdr = CorfuDBRuntime.getRuntime("http://localhost:9999/corfu");
-
-        cinstance = cdr.getLocalInstance();
+        opts.put(master,serviceURL);
+        cdbFactory=new CorfuDBFactory(opts);
+        cdr=cdbFactory.getRuntime();
+        cinstance=cdr.getLocalInstance();
         cinstance.getConfigurationMaster().resetAll();
+//        Map<String, Object> luConfigMap = new HashMap<String,Object>() {
+//            {
+//                put("capacity", 100000);
+//                put("ramdisk", true);
+//                put("pagesize", 4096);
+//                put("trim", 0);
+//            }
+//        };
+//        CorfuInfrastructureBuilder cbuilder = new CorfuInfrastructureBuilder().
+//                addLoggingUnit(7002, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
+//                addLoggingUnit(7003, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
+//                addLoggingUnit(7004, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
+//                addSequencer(7001, NettyStreamingSequencerServer.class, "nsss", null).
+//                //              setReplication("cdbqr").
+//                start(9999) ;
+//
+//        cdr = CorfuDBRuntime.getRuntime("http://localhost:9999/corfu");
+//
+//        cinstance = cdr.getLocalInstance();
+//        cinstance.getConfigurationMaster().resetAll();
     }
 
     public void appendData(UUID streamID, ICorfuDBInstance instance) throws IOException {
-        //StreamingSequencer cfstreaming = new StreamingSequencer(client,streamID);
-        //SimpleStream cfstream=new SimpleStream(streamID,instance);
-        //SimpleLog cflog=new SimpleLog();
-        IStream iStream = instance.openStream(streamID);
-        //NewStream cfstream=new NewStream(streamID,instance);
-        int worksize = 10000;
-        //long nextPos=cfstreaming.getNext(streamID, worksize);
-        //ITimestamp timestamp=cfstream.getFirstTimestamp();
-        //System.out.println(cfstream.getFirstTimestamp());
+
+        //IStream iStream = cinstance.openStream(streamID);
+        NewStream newStream=new NewStream(streamID,cinstance);
+        int worksize = 1;
+
         for (int j = 0; j < worksize; j++) {
-            iStream.append("i".getBytes());
+            //iStream.append("i".getBytes());
+            newStream.append(j);
+
         }
-//        int datacount=0;
-//        int leftsize=worksize;
-//        while (datacount<worksize){
-//            for (int i = 0; i < leftsize; i++) {
-//                try {
-//                    iStream.append(i);
-//                    datacount++;
-//                    if(i==9999){
-//                        leftsize=leftsize-i;
-//                        i=0;
-//                        instance.getConfigurationMaster().resetAll();
-//                    }
-//                } catch (IOException e) {
-//                    System.out.println("nihao");
-//                    e.printStackTrace();
-//                }
-//            }
-//        }
+
 
     }
 
