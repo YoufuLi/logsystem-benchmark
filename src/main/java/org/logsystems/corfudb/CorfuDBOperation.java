f7b5a733bbc1e81ccebc930a441e22276032cbe1
youfuli <youfuli.fly@gmail.com>
2015-11-05 21:37:07 -0800

    minor modification

--- src/main/java/org/logsystems/corfudb/CorfuDBOperation.java
+++ src/main/java/org/logsystems/corfudb/CorfuDBOperation.java
@@ -1,10 +1,13 @@
 package org.logsystems.corfudb;
 
+import org.corfudb.infrastructure.NettyLogUnitServer;
+import org.corfudb.infrastructure.NettyStreamingSequencerServer;
 import org.corfudb.runtime.CorfuDBRuntime;
 
 import org.corfudb.runtime.stream.*;
 import org.corfudb.runtime.view.*;
 import org.corfudb.util.CorfuDBFactory;
+import org.corfudb.util.CorfuInfrastructureBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,42 +26,42 @@ public class CorfuDBOperation {
     CorfuDBRuntime cdr;
     ICorfuDBInstance cinstance;
     public CorfuDBOperation(String master, String serviceURL){
-        opts.put(master,serviceURL);
-        cdbFactory=new CorfuDBFactory(opts);
-        cdr=cdbFactory.getRuntime();
-        cinstance=cdr.getLocalInstance();
-        cinstance.getConfigurationMaster().resetAll();
-//        Map<String, Object> luConfigMap = new HashMap<String,Object>() {
-//            {
-//                put("capacity", 100000);
-//                put("ramdisk", true);
-//                put("pagesize", 4096);
-//                put("trim", 0);
-//            }
-//        };
-//        CorfuInfrastructureBuilder cbuilder = new CorfuInfrastructureBuilder().
-//                addLoggingUnit(7002, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
-//                addLoggingUnit(7003, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
-//                addLoggingUnit(7004, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
-//                addSequencer(7001, NettyStreamingSequencerServer.class, "nsss", null).
-//                //              setReplication("cdbqr").
-//                start(9999) ;
-//
-//        cdr = CorfuDBRuntime.getRuntime("http://localhost:9999/corfu");
-//
-//        cinstance = cdr.getLocalInstance();
+//        opts.put(master,serviceURL);
+//        cdbFactory=new CorfuDBFactory(opts);
+//        cdr=cdbFactory.getRuntime();
+//        cinstance=cdr.getLocalInstance();
 //        cinstance.getConfigurationMaster().resetAll();
+        Map<String, Object> luConfigMap = new HashMap<String,Object>() {
+            {
+                put("capacity", 100000);
+                put("ramdisk", true);
+                put("pagesize", 4096);
+                put("trim", 0);
+            }
+        };
+        CorfuInfrastructureBuilder cbuilder = new CorfuInfrastructureBuilder().
+                addLoggingUnit(7002, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
+                addLoggingUnit(7003, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
+                addLoggingUnit(7004, 0, NettyLogUnitServer.class, "nlu", luConfigMap).
+                addSequencer(7001, NettyStreamingSequencerServer.class, "nsss", null).
+                //              setReplication("cdbqr").
+                start(9999) ;
+
+        cdr = CorfuDBRuntime.getRuntime("http://localhost:9999/corfu");
+
+        cinstance = cdr.getLocalInstance();
+        cinstance.getConfigurationMaster().resetAll();
     }
 
     public void appendData(UUID streamID, ICorfuDBInstance instance) throws IOException {
-
-        //IStream iStream = cinstance.openStream(streamID);
-        NewStream newStream=new NewStream(streamID,cinstance);
+        instance.getNewStreamingSequencer();
+        IStream iStream = instance.openStream(streamID);
+        //NewStream newStream=new NewStream(streamID,cinstance);
         int worksize = 1;
 
         for (int j = 0; j < worksize; j++) {
-            //iStream.append("i".getBytes());
-            newStream.append(j);
+            iStream.append("i".getBytes());
+            //newStream.append(j);
 
         }
 
